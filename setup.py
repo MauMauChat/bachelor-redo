2704efdd258aec1c014ded96bd1edaf63dd2bc8e
MauMauChat <lucaan@edu.aau.at>
2025-03-06 16:59:31 +0100

    Add requirements.txt

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Installation und Setup-Skript
-from setuptools import setup, find_packages
-
-setup(
-    name="Projektname",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[],
-)
